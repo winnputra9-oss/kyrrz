b71c0769a40f8f7ee3f656879eaa79eb60e92af0
winnputra9-oss <winnputra9@gmail.com>
2026-06-02 15:36:30 +0700

    Update naze.js

--- naze.js
+++ naze.js
@@ -4083,7 +4083,7 @@ Select Bot Settings:
 ├ *Money* : ${db.users[m.sender] ? db.users[m.sender].money.toLocaleString('id-ID') : '0'}
 ╰─┬────❍
 ╭─┴─❍「 *BOT INFO* 」❍
-├ *Nama Bot* : ${set?.botname || 'Naze Bot'}
+├ *Nama Bot* : ${set?.botname || 'Kay Bot'}
 ├ *Powered* : @${'0@s.whatsapp.net'.split('@')[0]}
 ├ *Owner* : @${ownerNumber[0].split('@')[0]}
 ├ *Mode* : ${naze.public ? 'Public' : 'Self'}
